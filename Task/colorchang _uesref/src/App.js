0bdd4fce6e8aae3a4236d93c35228612ad13b808
PanchalJaimin229 <jaiminpanchal229@gmail.com>
2023-11-06 10:52:30 +0530

    .

---
+++ Task/colorchang _uesref/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Clickcolorchang from './Clickcolorchang'
+
+function App() {
+  return (
+    <div>
+    <Clickcolorchang/>
+    
+    </div>
+  )
+}
+
+export default App
